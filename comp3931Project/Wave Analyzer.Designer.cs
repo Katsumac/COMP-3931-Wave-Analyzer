bee0de3a1b9002002037a2ca7dbe57812385f367
ldiu <liana.diu@hotmail.com>
2023-12-08 21:40:14 -0800

    added another event in case

--- comp3931Project/Wave Analyzer.Designer.cs
+++ comp3931Project/Wave Analyzer.Designer.cs
@@ -44,8 +44,8 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, toolsToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Padding = new Padding(6, 3, 0, 3);
-            menuStrip1.Size = new Size(1262, 30);
+            menuStrip1.Padding = new Padding(5, 2, 0, 2);
+            menuStrip1.Size = new Size(1104, 24);
             menuStrip1.TabIndex = 1;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -53,20 +53,20 @@
             // 
             fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { FileOpenToolStripMenuItem, saveToAudioFileToolStripMenuItem });
             fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Size = new Size(37, 20);
             fileToolStripMenuItem.Text = "File";
             // 
             // FileOpenToolStripMenuItem
             // 
             FileOpenToolStripMenuItem.Name = "FileOpenToolStripMenuItem";
-            FileOpenToolStripMenuItem.Size = new Size(224, 26);
+            FileOpenToolStripMenuItem.Size = new Size(168, 22);
             FileOpenToolStripMenuItem.Text = "Open";
             FileOpenToolStripMenuItem.Click += FileOpenToolStripMenuItem_Click;
             // 
             // saveToAudioFileToolStripMenuItem
             // 
             saveToAudioFileToolStripMenuItem.Name = "saveToAudioFileToolStripMenuItem";
-            saveToAudioFileToolStripMenuItem.Size = new Size(224, 26);
+            saveToAudioFileToolStripMenuItem.Size = new Size(168, 22);
             saveToAudioFileToolStripMenuItem.Text = "Save to Audio File";
             saveToAudioFileToolStripMenuItem.Click += saveToAudioFileToolStripMenuItem_Click;
             // 
@@ -74,33 +74,35 @@
             // 
             toolsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ToolRecordButton });
             toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
-            toolsToolStripMenuItem.Size = new Size(58, 24);
+            toolsToolStripMenuItem.Size = new Size(46, 20);
             toolsToolStripMenuItem.Text = "Tools";
+            toolsToolStripMenuItem.Click += toolsToolStripMenuItem_Click;
             // 
             // ToolRecordButton
             // 
             ToolRecordButton.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem });
             ToolRecordButton.Name = "ToolRecordButton";
-            ToolRecordButton.Size = new Size(224, 26);
+            ToolRecordButton.Size = new Size(180, 22);
             ToolRecordButton.Text = "Record";
             ToolRecordButton.MouseEnter += ToolRecordButton_MouseEnter;
             // 
             // newToolStripMenuItem
             // 
             newToolStripMenuItem.Name = "newToolStripMenuItem";
-            newToolStripMenuItem.Size = new Size(224, 26);
+            newToolStripMenuItem.Size = new Size(98, 22);
             newToolStripMenuItem.Text = "New";
             newToolStripMenuItem.Click += newToolStripMenuItem_Click;
             // 
             // WaveAnalyzer
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Navy;
-            ClientSize = new Size(1262, 1055);
+            ClientSize = new Size(1104, 791);
             Controls.Add(menuStrip1);
             IsMdiContainer = true;
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "WaveAnalyzer";
             Text = "Wave Analyzer";
             menuStrip1.ResumeLayout(false);
